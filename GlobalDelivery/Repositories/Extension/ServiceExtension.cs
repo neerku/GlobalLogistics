07c74c7fea601b2fcf102720316968113729f2a2
neeraj <neerku>
2020-11-06 15:26:54 +0530

    fixed bugs

--- GlobalDelivery/Repositories/Extension/ServiceExtension.cs
+++ GlobalDelivery/Repositories/Extension/ServiceExtension.cs
@@ -24,9 +24,9 @@ namespace GlobalDelivery.Repositories.Extension
             services.AddSingleton<CargoRepository>();
             services.AddSingleton<CityRepository>();
 
-            var wa = new WatchStream(client);
-            Task.Run(() => wa.StartCollectionWatch()).ConfigureAwait(false);
-            Task.Run(() => wa.AssignPlaneToCargo()).ConfigureAwait(false);
+            //var wa = new WatchStream(client);
+            //Task.Run(() => wa.StartCollectionWatch()).ConfigureAwait(false);
+            //Task.Run(() => wa.AssignPlaneToCargo()).ConfigureAwait(false);
         }
     }
 }
